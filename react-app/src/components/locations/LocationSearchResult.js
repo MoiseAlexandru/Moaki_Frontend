4ab1e1708c7ee5e1becef6ffe5ed6234fce3217a
anne <messnerbeatrix@gmail.com>
2023-12-06 20:20:07 +0200

    locations css

--- react-app/src/components/locations/LocationSearchResult.js
+++ react-app/src/components/locations/LocationSearchResult.js
@@ -1,7 +1,23 @@
 
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button";
+import "../../css/locationsSearchResults.css"
 export default function LocationSearchResult({location}) {
     return (
-        <div> {location.name} </div>
+        <Container className = "locationContainer">
+        <Row className = "locationName"><h3> {location.name} </h3></Row>
+        <Row>
+            <Col className = "locationCity"><h4>City: {location.city}</h4></Col>
+            <Col className = "locationCountry"><h4>Country: {location.country}</h4></Col>
+        </Row>
+        <Row>
+            <Col className = "locationReviews"><h4>Reviews: {location.reviews}</h4></Col>
+            <Col className = "locationRating"><h4>Rating: {location.ratings}</h4></Col>
+        </Row>
+        <Row><Button variant="viewLocationButton">See posts! </Button></Row>
+        </Container>
+        
     ); 
 }
