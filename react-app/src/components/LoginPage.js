ab07372442aa88b5b460610133b5a73a4ae7f7ac
unknown <moise.alexandru.ichb@gmail.com>
2024-01-15 20:04:11 +0200

    adaugat fielduri pt login

--- react-app/src/components/LoginPage.js
+++ react-app/src/components/LoginPage.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 
-export default function LoginPage() {
+export default function LoginPage({onLogin}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div>
-            Login page
+            <input type = "text" onChange = {(e) => setUsername(e.target.value)} placeholder = "username" />
+            <input type = "password" onChange = {(e) => setPassword(e.target.value)} placeholder="password" />
+            <button onClick={onLogin}> Login </button>
         </div>
     )
 }
